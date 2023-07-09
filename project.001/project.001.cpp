74be966daa0706e8ab9bb4e3fbb9b8e231a8d25c
Alice(AbdushaevDA) <abdushaev04@list.ru>
2023-07-09 13:11:23 +0300

    code: Добавлен вывод максимума

--- project.001/project.001.cpp
+++ project.001/project.001.cpp
@@ -1,5 +1,13 @@
 ﻿#include <iostream>
 using namespace std;
+int max(int a, int b) {
+	int max=0;
+	if (a > b)
+		max = a;
+	else
+		max = b;
+	return max;
+}
 int main()
 {
 	cout << "Enter A and B: ";
@@ -9,4 +17,5 @@ int main()
 	cout << "A-B= " << a - b << '\n';
 	cout << "A*B= " << a * b << '\n';
 	cout << "A/B= " << a / b << '\n';
+	cout << "Max= " << max(a, b) << '\n';
 }
